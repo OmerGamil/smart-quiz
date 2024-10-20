8426bec7cff7e20bb1babbaeea6e82eb7a627f4c
Omer Ahmed <omer.gamil200@gmail.com>
2024-10-20 21:27:27 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,41 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+QUIZ_QUESTIONS = [
+    {
+        "question": "What is the capital of England?",
+        "choices": ["Birmingham", "Paris", "London", "Tokoyo"],
+        "correct": 2
+    },
+    {
+        "question": "What is the capital of France?",
+        "choices": ["Birmingham", "Paris", "London", "Tokoyo"],
+        "correct": 1
+    },
+]
+
+username = ""
+question_number = 0
+score = 0
+
+def welcome():
+    """
+    Welcomes user and asks for name.
+    """
+    print("Welcome! to my very short quiz\n")
+
+    global username
+    while username == "":
+        username = input("Please enter your name: ").strip()
+        if username == "":
+            print("No, you must enter something!")
+
+    print("\nHello, " + username)
+
+
+def main():
+    """
+    Run all program functions.
+    """
+    welcome()
+    ask_question()
+    check_answer(answer)
+
+main()
