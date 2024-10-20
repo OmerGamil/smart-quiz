c02697301a1cdb73aaa01efc743a7fc86c9ee4af
Omer Ahmed <omer.gamil200@gmail.com>
2024-10-20 21:30:33 +0000

    Add ask question finction to ask the user the QUIZ's  questions

--- run.py
+++ run.py
@@ -30,6 +30,22 @@ def welcome():
     print("\nHello, " + username)
 
 
+def ask_question():
+    """
+    Get the questions according to their order.
+    """
+    print("Get ready for the QUIZ...")
+
+    quest = QUIZ_QUESTIONS[question_number]
+    print("\n" + quest["question"])
+
+    for i, choice in enumerate(quest["choices"], 1):
+        print(f"{i}. {choice}")  # Print the choices with numbering
+
+    global answer
+    answer = int(input("\n: ").strip()) - 1 # Substract the answer to get the right index
+
+
 def main():
     """
     Run all program functions.
